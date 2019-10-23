01fcebe1db6cf5046131a0e92c1895c7834c23fe
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-10-23 14:02:55 +0200

    Track class added

---
+++ AirTrafficMonitoring/AirTrafficMonitoring/Track.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoring
+{
+    // <summary>
+    // Track is a data class contaning all information of a track
+    // Track will not have any methods due to public properties
+    // </summary>
+    public class Track
+    {
+        public string Tag { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Z { get; set; }
+
+        public int CompassCourse { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
