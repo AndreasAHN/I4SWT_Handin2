5cc121e09c7ec81dcf8d2959d4b14c045d6bda5c
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-10-24 12:15:59 +0200

    TransponderReceiverClient added

---
+++ AirTrafficMonitoring/AirTrafficMonitoring/TransponderReceiverClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace AirTrafficMonitoring
+{
+    public class TransponderReceiverClient
+    {
+        private readonly ITransponderReceiver _receiver;
+
+        public event EventHandler DataReceivedEvent;
+
+        public Track[] Tracks { get; private set; }
+
+        public TransponderReceiverClient(ITransponderReceiver receiver)
+        {
+            _receiver = receiver;
+            _receiver.TransponderDataReady += HandleTransponderDataReady;
+        }
+
+        private void HandleTransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            Tracks = new Track[e.TransponderData.Count];
+
+            string[] tokens;
+            char[] separator = {';'};
+
+            int trackIndex = 0;
+
+            foreach (var data in e.TransponderData)
+            {
+                tokens = data.Split(separator);
+                Tracks[trackIndex] = new Track()
+                {
+                    Tag = tokens[0],
+                    X = int.Parse(tokens[1]),
+                    Y = int.Parse(tokens[2]),
+                    Z = int.Parse(tokens[3]),
+                    Timestamp = DateTime.ParseExact(tokens[4], "yyyyMMddHHmmssfff", null)
+                };
+                trackIndex++;
+            }
+            DataReceivedEvent?.Invoke(null, null);
+        }
+    }
+}
