03cba87e5f0d9bbec3df163d1fc39adada5b8594
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-10-24 16:10:32 +0200

    TrackCalculator_test Basic setup

--- AirTrafficMonitoring/AirTrafficMonitoring_Tester/TrackCalculator_Test.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring_Tester/TrackCalculator_Test.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitoring;
-using NUnit.Framework.Internal;
+using NUnit.Framework;
 
 namespace AirTrafficMonitoring_Tester
 {
@@ -12,6 +12,41 @@ namespace AirTrafficMonitoring_Tester
     public class TrackCalculator_Test
     {
         private TrackCalculator _uut;
-        
+        private Track _oldTrack;
+        private Track _newTrack;
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new TrackCalculator();
+
+            _oldTrack = new Track()
+            {
+                Tag = "JYG338",
+                X = 5258,
+                Y = 57189,
+                Z = 5000,
+                Timestamp = DateTime.ParseExact("20191024155401709", "yyyyMMddHHmmssfff", null)
+            };
+
+            _newTrack = new Track()
+            {
+                Tag = "JYG338",
+                X = 5387,
+                Y = 57076,
+                Z = 5000,
+                Timestamp = DateTime.ParseExact("20191024155402397", "yyyyMMddHHmmssfff", null)
+            };
+        }
+
+        [Test]
+        public void CalculateVelocity()
+        {
+            
+        }
+
+
+
+
+
     }
 }
