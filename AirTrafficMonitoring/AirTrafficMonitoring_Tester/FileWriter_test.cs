eb2d58b9090da260795bdaea65647f01a8e839fa
Ronnie Jakobsgaard <ronnie192@gmail.com>
2019-10-29 17:33:16 +0100

    Edit for FileWriterTest, removed SystemTime class

--- AirTrafficMonitoring/AirTrafficMonitoring_Tester/FileWriter_test.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring_Tester/FileWriter_test.cs
@@ -7,33 +7,11 @@ using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitoring;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace AirTrafficMonitoring_Tester
 {
-    /// <summary>
-    /// Used for getting DateTime.Now(), time is changeable for unit testing
-    /// </summary>
-    public static class SystemTime
-    {
-        /// <summary> Normally this is a pass-through to DateTime.Now, but it can be overridden with SetDateTime( .. ) for testing or debugging.
-        /// </summary>
-        public static Func<DateTime> Now = () => DateTime.Now;
-
-        /// <summary> Set time to return when SystemTime.Now() is called.
-        /// </summary>
-        public static void SetDateTime(DateTime dateTimeNow)
-        {
-            Now = () => dateTimeNow;
-        }
-
-        /// <summary> Resets SystemTime.Now() to return DateTime.Now.
-        /// </summary>
-        public static void ResetDateTime()
-        {
-            Now = () => DateTime.Now;
-        }
-    }
     [TestFixture]
     public class FileWriter_test
     {
@@ -83,6 +61,8 @@ namespace AirTrafficMonitoring_Tester
             {
                 Tag = "Tag2"
             };
+
+
 
             // Write to file
             _uut.WriteToLog(t1, t2);
