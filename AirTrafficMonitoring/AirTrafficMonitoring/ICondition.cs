1158fe756120b3350ea9805e4618f202a838416f
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-10-30 13:32:18 +0100

    ICondition

---
+++ AirTrafficMonitoring/AirTrafficMonitoring/ICondition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoring
+{
+    public interface ICondition
+    {
+        bool TooClose(List<Track> tracks);
+        bool GetSeperation();
+        List<Track> GetConflictAirplain1();
+        List<Track> GetConflictAirplain2();
+    }
+}
