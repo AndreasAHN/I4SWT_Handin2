58487274b7f4c9a11d2e17a6f3ebc626d3a7616b
Malou <maloutroelsen@gmail.com>
2019-10-30 17:01:55 +0100

    Hello Coverage

--- AirTrafficMonitoring/AirTrafficMonitoring_Tester/TrackCalculator_Test.cs
+++ AirTrafficMonitoring/AirTrafficMonitoring_Tester/TrackCalculator_Test.cs
@@ -16,6 +16,7 @@ namespace AirTrafficMonitoring_Tester
         private TrackCalculator _uut;
         private List<Track> _testTracksOld;
         private List<Track> _testTracksNew;
+        private List<Track> _testTracks;
 
         //private Track _oldTrack;
         //private static Track _newTrack;
@@ -28,18 +29,40 @@ namespace AirTrafficMonitoring_Tester
 
             _testTracksOld = new List<Track>();
             _testTracksNew = new List<Track>();
+            _testTracks = new List<Track>();
 
-            _testTracksOld.Add(new Track { Tag = "JYG338", X = 5258, Y = 57189, Z = 5000, Timestamp = DateTime.ParseExact("20191024155401709", "yyyyMMddHHmmssfff", null) });
-            _testTracksOld.Add(new Track { Tag = "GVC241", X = 38594, Y = 77966, Z = 10900, Timestamp = DateTime.ParseExact("20191024155401709", "yyyyMMddHHmmssfff", null) });
-            _testTracksOld.Add(new Track { Tag = "WIA512",X = 17357, Y = 24364, Z = 2100, Timestamp = DateTime.ParseExact("20191024155401709", "yyyyMMddHHmmssfff", null) });
+            _testTracksOld.Add(new Track { Tag = "JYG338", X = 5258, Y = 57189, Z = 5000, Timestamp = DateTime.ParseExact("20191024155401709", "yyyyMMddHHmmssfff", null), CompassCourse = 0, Velocity = 0});
+            _testTracksOld.Add(new Track { Tag = "GVC241", X = 38594, Y = 77966, Z = 10900, Timestamp = DateTime.ParseExact("20191024155401709", "yyyyMMddHHmmssfff", null), CompassCourse = 0, Velocity = 0 });
+            _testTracksOld.Add(new Track { Tag = "WIA512",X = 17357, Y = 24364, Z = 2100, Timestamp = DateTime.ParseExact("20191024155401709", "yyyyMMddHHmmssfff", null), CompassCourse = 0, Velocity = 0 });
 
-            _testTracksNew.Add(new Track { Tag = "JYG338", X = 5387, Y = 57076, Z = 5000, Timestamp = DateTime.ParseExact("20191024155402397", "yyyyMMddHHmmssfff", null) });
-            _testTracksNew.Add(new Track { Tag = "GVC241", X = 38794, Y = 78066, Z = 10800, Timestamp = DateTime.ParseExact("20191024155402397", "yyyyMMddHHmmssfff", null) });
-            _testTracksNew.Add( new Track { Tag = "WIA512", X = 18357, Y = 25364, Z = 2000, Timestamp = DateTime.ParseExact("20191024155402397", "yyyyMMddHHmmssfff", null) });
+            _testTracksNew.Add(new Track { Tag = "JYG338", X = 5387, Y = 57076, Z = 5000, Timestamp = DateTime.ParseExact("20191024155402397", "yyyyMMddHHmmssfff", null), CompassCourse = 0, Velocity = 0 });
+            _testTracksNew.Add(new Track { Tag = "GVC241", X = 38794, Y = 78066, Z = 10800, Timestamp = DateTime.ParseExact("20191024155402397", "yyyyMMddHHmmssfff", null), CompassCourse = 0, Velocity = 0 });
+            _testTracksNew.Add( new Track { Tag = "WIA512", X = 18357, Y = 25364, Z = 2000, Timestamp = DateTime.ParseExact("20191024155402397", "yyyyMMddHHmmssfff", null), CompassCourse = 0, Velocity = 0 });
+
+            _testTracks.Add(_testTracksOld[0]);
+            _testTracks.Add(_testTracksNew[0]);
+            _testTracks.Add(_testTracksOld[1]);
+            _testTracks.Add(_testTracksNew[1]);
+            _testTracks.Add(_testTracksOld[2]);
+            _testTracks.Add(_testTracksNew[2]);
         }
 
+
+
+        //[Test]
+        //public void Test_TrackCalculate()
+        //{
+        //    var test0 = _uut.TrackCalculate(_testTracksOld);
+        //    Track resTrack1 = new Track { Tag = "JYG338", X = 5258, Y = 57189, Z = 5000, Timestamp = DateTime.ParseExact("20191024155401709", "yyyyMMddHHmmssfff", null), CompassCourse = 0, Velocity = 0 };
+        //    Assert.AreEqual(resTrack1, test0[0]);
+
+        //    var test1 = _uut.TrackCalculate(_testTracksNew);
+        //    Track resTrack2 = new Track { Tag = "JYG338", X = 5387, Y = 57076, Z = 5000, Timestamp = DateTime.ParseExact("20191024155402397", "yyyyMMddHHmmssfff", null), CompassCourse = 249, Velocity = 318};
+        //    Assert.AreEqual(resTrack2, test1[0]);
+        //}
+
         [Test]
-        public void Test_Velocity_Calc()
+        public void Test_CalculateVelocity()
         {
             var test0 = _uut.CalculateVelocity(_testTracksNew[0], _testTracksOld[0]);
             var test1 = _uut.CalculateVelocity(_testTracksNew[1], _testTracksOld[1]);
@@ -50,7 +73,7 @@ namespace AirTrafficMonitoring_Tester
         }
 
         [Test]
-        public void Test_Course_Calc()
+        public void Test_CalculateCompassCourse()
         {
             var test0 = _uut.CalculateCompassCourse(_testTracksNew[0], _testTracksOld[0]);
             var test1 = _uut.CalculateCompassCourse(_testTracksNew[1], _testTracksOld[1]);
