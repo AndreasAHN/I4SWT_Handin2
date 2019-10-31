2c68dc68fea95b0f1768070f90b87b433f5a8836
Andreas Hervert Nielsen <andreas.hervert@gmail.com>
2019-10-31 02:17:59 +0100

    screen test

--- AirTrafficMonitoring_Tester/Screen_Test.cs
+++ AirTrafficMonitoring_Tester/Screen_Test.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 using AirTrafficMonitoring;
 using NSubstitute;
 using NUnit.Framework;
+
 
 namespace AirTrafficMonitoring_Tester
 {
@@ -28,8 +30,19 @@ namespace AirTrafficMonitoring_Tester
             _testData.Add(new Track { Tag = "QRM275", X = 31268, Y = 57982, Z = 600, Timestamp = DateTime.Now });
             _testData.Add(new Track { Tag = "ONC788", X = 28636, Y = 26560, Z = 500, Timestamp = DateTime.Now });
 
+            var consolReader = new Process();
+            consolReader.StartInfo.UseShellExecute = false;
+            consolReader.StartInfo.RedirectStandardOutput = true;
+            consolReader.StartInfo.FileName = "csc.exe";
+            consolReader.Start();
+
             Screen screen = new Screen();
             screen.printTracks(_testData);
+
+            string result = consolReader.StandardOutput.ReadToEnd();
+            consolReader.WaitForExit();
+
+            Console.WriteLine(result);
         }
 
         [Test]
@@ -38,8 +51,19 @@ namespace AirTrafficMonitoring_Tester
             Track _testData1 = (new Track { Tag = "GPJ740", X = 46155, Y = 16263, Z = 3500, Timestamp = DateTime.Now });
             Track _testData2 = (new Track { Tag = "QRM275", X = 31268, Y = 57982, Z = 600, Timestamp = DateTime.Now });
 
+            var consolReader = new Process();
+            consolReader.StartInfo.UseShellExecute = false;
+            consolReader.StartInfo.RedirectStandardOutput = true;
+            consolReader.StartInfo.FileName = "csc.exe";
+            consolReader.Start();
+
             Screen screen = new Screen();
             screen.printConflict(_testData1, _testData2);
+
+            string result = consolReader.StandardOutput.ReadToEnd();
+            consolReader.WaitForExit();
+
+            Console.WriteLine(result);
         }
     }
 }
